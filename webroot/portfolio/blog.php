a6e5326a3cc40ba9cf33ed39417b8e27a50a20d5
saeed439 <60136878+saeed439@users.noreply.github.com>
2020-05-01 16:35:12 -0700

    changed

--- webroot/portfolio/blog.php
+++ webroot/portfolio/blog.php
@@ -4,6 +4,28 @@
 <html>
 <head>
 <title>Blog</title>
+
+ <style>
+
+table{
+border-collapse: collapse;
+width:100%;
+color:#d96444;
+font-family: monospace;
+font-size: 25px;
+text-align: left;
+}
+
+th{
+background-color:#d96444;
+color:white;
+}
+tr:nth-child {
+background-color: #f2f2f2
+}
+
+
+</style>
 </head>
 <body>
 
