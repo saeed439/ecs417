1f7a19e2e779e5629bd05c41a95271032390c6ac
saeed439 <60136878+saeed439@users.noreply.github.com>
2020-05-02 05:31:40 -0700

    changed

---
+++ webroot/portfolio/signup.php
@@ -0,0 +1,87 @@
+<?php 
+ SESSION_START();
+ $dbhost= getenv("MYSQL_SERVICE_HOST");
+ $dbport= getenv("MYSQL_SERVICE_PORT");
+ $dbuser= getenv("DATABASE_USER");
+ $password= getenv("DATABASE_PASSWORD");
+ $dbname= getenv("DATABASE_NAME");
+
+ 
+ $conn = new mysqli($dbhost, $dbuser, $password, $dbname);
+ if ($conn->connect_error){
+      die("connection failed: " . $conn->connect_error);
+
+                           }
+
+
+
+ if(isset($_POST['submit']))
+
+{
+
+   $title=$_POST['username'];
+   $comment=$_POST['password'];
+
+$sql =  "INSERT INTO USER (username, password)
+          VALUES ('$title' , '$comment')";
+
+
+$sql2= mysqli_query($conn, $sql);
+
+ if ($sql2){
+
+  header('Location:logout.php');
+}
+ 
+else {
+
+  echo "fail";
+}
+
+
+
+  
+}
+
+  
+
+
+
+ $conn->close();
+
+
+
+?>
+<!doctype html>
+<html>
+
+<head>
+<link rel="stylesheet" href="reset2.css" type="text/css" />
+<link rel="stylesheet" href="addentrycss.css" type="text/css" />
+</head>
+
+<body>
+
+
+<form method="POST" action="#">
+ <legend>Create Your User and Pass</legend>
+
+ <section id="section1">
+ <label for="username">username</label>
+ <div>
+ <input type="text" name="username" id="pass" required>
+<div>
+ <label for="password">password</label>
+<div>
+ <input type="password" name="password" id="pass" required>
+<div>
+</section>
+<section id="buttons">
+
+<input type="submit" value="signup" id="button">
+</section>
+</form>
+
+</body>
+
+</html>
