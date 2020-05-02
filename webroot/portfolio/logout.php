82042b7c4dd7261ff17189046b547fe0417e9237
saeed439 <60136878+saeed439@users.noreply.github.com>
2020-05-01 18:13:45 -0700

    changed

--- webroot/portfolio/logout.php
+++ webroot/portfolio/logout.php
@@ -6,7 +6,7 @@ $_SESSION = array();
 session_destroy();
 
 
-
+header('Location:exercise2week5.html');
 ?>
 
 
@@ -14,37 +14,3 @@ session_destroy();
 
 
 
-<html>
-
-<head>
-<link rel="stylesheet" href="reset2.css" type="text/css" />
-<link rel="stylesheet" href="addentrycss.css" type="text/css" />
-</head>
-
-<body>
-
-
-<form>
-
- <legend>Thank you for adding your comment</legend>
-
-<section>
-<?php 
-   echo "CURRENT DATE ".date("m-d-y");
-   ?>
-<br>
-
-<?php 
-echo "CURRENT TIME ".date("h:i:s");
-?>
-</section>
-<section id="buttons">
-<a href="blog.php" ><button id="button">BLOG</button></a>
-
-<a href="exercise2week5.html"><button id="button">HOME</button></a>
-</section>
-</form>
-
-</body>
-
-</html>
