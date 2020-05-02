e247e9124460c3ad8a0108eb705c3dfe9146792c
saeed439 <60136878+saeed439@users.noreply.github.com>
2020-05-01 17:40:58 -0700

    changed

---
+++ webroot/portfolio/logout.php
@@ -0,0 +1,46 @@
+<?php
+SESSION_START();
+
+$_SESSION = array();
+
+session_destroy();
+
+
+
+?>
+
+
+
+
+
+
+<html>
+
+<head>
+<link rel="stylesheet" href="reset2.css" type="text/css" />
+<link rel="stylesheet" href="addentrycss.css" type="text/css" />
+</head>
+
+<body>
+
+
+<form action="#">
+
+ <legend>Thank you for adding your comment</legend>
+
+<section>
+<?php 
+   echo "CURRENT DATE ".date("m-d-y");
+   echo "CURRENT TIME ".date("h:i:s");
+?>
+</section>
+<section id="buttons">
+<a href="blog.php" ><button id="button">BLOG</button></a>
+
+<a href="exercise2week5.html"><button id="button">HOME</button></a>
+</section>
+</form>
+
+</body>
+
+</html>
