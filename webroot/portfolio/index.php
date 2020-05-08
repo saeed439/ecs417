02331b32a2433715ba20fdf3571ff5cac252e428
saeed439 <60136878+saeed439@users.noreply.github.com>
2020-05-07 18:16:01 -0700

    changed

--- webroot/portfolio/index.php
+++ webroot/portfolio/index.php
@@ -1,4 +1,4 @@
- <!doctype html>
+<!doctype html>
 <html>
 <head>
 <title> Portfolio </title>
@@ -17,18 +17,18 @@
 	<h1><a id="header">Saeed Kaddoura</a><h1>
 
 	<a id="navelements" href="#hobbies">Contact</a>
-
-	<a  id="navelements" href="#education">Education</a>
-
- 	<a  id="navelements" href="#links">Blog</a>
-
+        <a  id="navelements" href="#education">Education</a>
+        <a  id="navelements" href="#links">Blog</a>
+        <a  id="navelements" href="#commentsection">Comment Section</a>
+        <a  id="navelements" href="#aboutmuself">About Myself</a>
+        <a  id="navelements" href="#skills">Skills</a>
 	</section>
 	
 </nav>
 <section id="body">
 <section id="mainbody">
 <hgroup>
-<h2><a name="About myself">About myself </a></h2>
+<h2><a name="aboutmuself">About Myself </a></h2>
 <section>
 <section id="pics">
 <figure  id="pic">
@@ -42,17 +42,38 @@ and im currently doing Computer science as a major.
 I enjoy reading and writing and doing many things in mmy spare times like spending time with my friends 
 and such. Since coding has always enthralled me and I have a deep interest in mathematics and logic, it is no surprise that I decided to study computer science at university. It has always been an ambition of mine to study computer science in the UK, as studying abroad is an academic and personal experience that offers multicultural interaction, elevates intellectual reasoning, whilst simultaneously appropriating personal growth and challenge. Furthermore, specializing in the major I'm immensely passionate about establishing deeper insight about the complex structures encountered in this field so that I could implement my acquired knowledge in my personal future projects. More precisely, I've chosen to study computer science at Queen Mary, in view of its high ranking not only in educational facets, but also in social activities according to alumni. 
 
-<br></p>
+<br><br><br></p>
+<h2><a name="About myself"> Oustide School </a></h2>
+<p>
+I decided to join the Arab and engineering society in queen mary as soon as i came to london, as this will help me socialise and feel more welcomed. This was more of socializing environment which I enjoyed more than the typical studying environment.
+This experience taught me how to approach certain social situations and that there are different personality types.
+In addition, i always like joining football teams, as i was always passionate about football.
+Joining Queen Mary social league was unlike any other experience in my home town. It was more exciting playing in my first league.
+
+
+
+
+
+
+
+
+</p>
 </article>
 </section>
 <section id="education" name="education">
 
 <h3 id="edutext"> Education </h3>
 <p id="educationtext">
- American High School Diploma 2016-2018
+ American High School Diploma with 3.9 gpa : 2016-2018
 <br>
 <br>
- Foundation Programe at Queen Mary 2018-2019
+ Foundation Programe at Queen Mary with Distiction: 2018-2019
+<br>
+<br>
+IELTS overall score : 7
+<br>
+<br>
+SAT score : 900+
 </p>
 </section>
 </section>
@@ -63,41 +84,71 @@ and such. Since coding has always enthralled me and I have a deep interest in ma
 <section id="research">
 <h4>Research Interests</h4>
 <p>
-Quantum Mechanics
+<br>
+Programing
 <div>
-Contect Awarness
+<br>
+Gamification
 <div>
-Math
+<br>
+Mathematics
 <div>
 </section>
 <section id="links" name="links">
-<a href="my-cv.html">cv</a>
+<a href="my-cv.html"><button>My Professional CV</button></a>
+<br>
+<br>
 <div>
-<a href="topic4.html">Previous project</a>
+<a href="topic4.html"><button>Previous Form I Created</button></a>
+<br>
+<br>
+<a href="exercise2.html"><button>My Favorite Movies</button></a>
 </section>
 <section id="bages2">
 <section id="badges">
-<h4 id="header3"> Top 3 skills</h4>
-<section id="badgesbackground">
+<h4 id="header3" > <a name="skills">Top 3 skills </a></h4>
+<section id="badgesbackground" >
 <p>
 <figure>
 <img src="first.png" >
 </figure>
 <div>
-<section id="badgefloat">Python code</section>
+<section id="badgefloat">Fluent in Java</section>
 <div>
 <figure>
 <img src="second.png" >
 </figure>
 <div>
-<section id="badgefloat">Html expert</section>
+<section id="badgefloat">Html Expert</section>
 <div>
 <figure>
 <img src="third.png" >
 </figure>
 <div>
-<section id="badgefloat">Honor Roll</section>
+<section id="badgefloat">Quick Learner</section>
 </p>
+</section>
+</section>
+<section id="badges">
+<h5 id="header3"><a name="hobbies">Other Skills</a></h5>
+<div>
+<section id="badgesbackground">
+
+  <p id="otherskills">
+  <ul>
+    <li class="otherskills">Billingual</li>
+    <li class="otherskills">Leader</li>
+    <li class="otherskills">Team Player</li>
+    <li class="otherskills">Good Speaker</li>
+    <li class="otherskills">Task Accomplisher</li>
+    <li class="otherskills">Fluent in Python</li>
+    <li class="otherskills">Web Designer</li>
+    <li class="otherskills">Good Writer</li>
+    <li class="otherskills">Debugging Expert</li>
+    <li class="otherskills">Problem Solving</li>
+  </ul>
+</p>
+
 </section>
 </section>
 </section>
@@ -109,13 +160,19 @@ Math
 <section id="badgesbackground">
 
 <p id="sidetext">
-mail:
+uni mail:
 <br>
 bt18673@qmul
+<br>
 <br>
 num:
 <br>
 07877858873
+<br>
+<br>
+personal mail:
+<br>
+saeed345345@
 </p>
 
 </section>
@@ -129,12 +186,20 @@ num:
 <br>
 <br>
 <a href="blog.php"><button>View My blog</button></a>
-<br>
-<br>
+
+</p>
+
+</section>
+</section>
+<section id="badges">
+<h5 id="header3"><a name="commentsection">Comment Section </a></h5>
+</hgroup>
+<section id="badgesbackground">
+<p id="commenttext">
 <a href="signup.php"><button> Sign up To Comment</button></a>
 <br>
 <br>
-<a href="adminlogin.php"><button> Admin Management</button></a>
+<a href="adminlogin.php"><button> Admin Comment Management</button></a>
 <br>
 <br>
 <a href="commentlogin.php"><button>Add Comment</button></a>
@@ -153,12 +218,12 @@ num:
 <footer>
 
 
-                 Copyright 2020 Saeed Kaddoura
+    <p id="footertext">             Copyright 2020 Saeed Kaddoura  <p>
 <br>
-<nav>
+<nav id="footernav">
 
-<a href="https://qmplus.qmul.ac.uk">My Student Study Source<a/> |
-<a href="https://www.library.qmul.ac.uk/">My Student Reading Source<a/>
+<a href="https://qmplus.qmul.ac.uk" id="footernav">My Student Study Source<a/> |
+<a href="https://www.library.qmul.ac.uk/" id="footernav">My Student Reading Source<a/>
 </nav>
 		</footer>
 
